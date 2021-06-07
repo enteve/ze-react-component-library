d84c2eb8fed113caa5a04f0c8d0b89a66681d56d
kilik52 <kilik52@hotmail.com>
2021-06-07 17:38:21 +0800

    fix some bugs

--- src/ZETable/ZETable.types.ts
+++ src/ZETable/ZETable.types.ts
@@ -9,12 +9,14 @@ export interface ZETableProps {
   titleMap?: { [key: string]: string }; // 字段在表格上显示什么样的标题
   customRender?: { [key: string]: Function }; // 自定义的render函数
   additionalColumns?: ProColumnType[]; // 在表格的最后添加各种自定义的列
-  options?: OptionConfig; // 传给ProTable的
-  scroll?: {
-    x?: string | number | true;
-    y?: string | number;
-  } & {
-    scrollToFirstRowOnChange?: boolean;
-  }; // 传给ProTable的。ZETable默认会有Scroll的。用null来关闭
+  options?: OptionConfig | false; // 传给ProTable的
+  scroll?:
+    | ({
+        x?: string | number | true;
+        y?: string | number;
+      } & {
+        scrollToFirstRowOnChange?: boolean;
+      })
+    | null; // 传给ProTable的。ZETable默认会有Scroll的。用null来关闭
   className?: string;
 }
