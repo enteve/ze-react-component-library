d122b549b380ab2f47bcd30fe8803625a133cf63
kilik52 <kilik52@hotmail.com>
2021-06-07 13:15:19 +0800

    ReadMe

--- src/ZETable/ZETable.types.ts
+++ src/ZETable/ZETable.types.ts
@@ -6,7 +6,7 @@ import type { LogicformType } from "zeroetp-api-sdk";
 export interface ZETableProps {
   logicform: LogicformType;
   preds?: string[]; // 显示哪些字段
-  customRender?: any; // 自定义的render函数
+  customRender?: { [key: string]: Function }; // 自定义的render函数
   additionalColumns?: ProColumnType[]; // 在表格的最后添加各种自定义的列
   options?: OptionConfig; // 传给ProTable的
 }
