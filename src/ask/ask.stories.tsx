016f7315497a09513b04a85c52b18b9cc8bf2dae
kilik52 <kilik52@hotmail.com>
2021-06-08 17:53:09 +0800

    ask function

---
+++ src/ask/ask.stories.tsx
@@ -0,0 +1,31 @@
+// Generated with util/create-component.js
+import { ArrowUpOutlined } from "@ant-design/icons";
+import { Card, Statistic } from "antd";
+import React from "react";
+import ask from "./index";
+
+export default {
+  title: "ZEAsk",
+};
+
+export const Ask = () => (
+  <>
+    <h2>近一年易问订单数量</h2>
+    <div>{ask("近一年易问订单数量")}</div>
+  </>
+);
+
+export const WithAntdCard = () => (
+  <>
+    <Card>
+      <Statistic
+        title="Active"
+        value={ask("近一年易问订单数量")}
+        precision={2}
+        valueStyle={{ color: "#3f8600" }}
+        prefix={<ArrowUpOutlined />}
+        suffix="%"
+      />
+    </Card>
+  </>
+);
---
+++ src/request.ts
@@ -0,0 +1,29 @@
+import { message } from "antd";
+import type {
+  LogicformType,
+  LogicformAPIResultType,
+  AskAPIResultType,
+} from "zeroetp-api-sdk";
+import { execLogicform, ask } from "zeroetp-api-sdk";
+
+export async function request(func: Promise<any>) {
+  try {
+    return await func;
+  } catch (error) {
+    message.error(error.message);
+  }
+
+  return null;
+}
+
+export async function requestAsk(question: string): Promise<AskAPIResultType> {
+  const ret = await request(ask(question, false));
+  return ret;
+}
+
+export async function requestLogicform(
+  logicform: LogicformType
+): Promise<LogicformAPIResultType> {
+  const ret = (await request(execLogicform(logicform))) || { result: [] };
+  return ret;
+}
