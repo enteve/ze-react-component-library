eec403ca8aae64d20d0974cbb993d3ee0ae18b54
kilik52 <kilik52@hotmail.com>
2021-06-16 23:48:13 +0800

    testcase: StatsAndCalcInFront

--- src/ZETable/ZETable.stories.tsx
+++ src/ZETable/ZETable.stories.tsx
@@ -137,3 +137,21 @@ export const Stats = () => (
     options={false}
   />
 );
+
+export const StatsAndCalcInFront = () => (
+  <ZETable
+    logicform={{
+      schema: "order",
+      preds: [
+        { name: "销量", operator: "$sum", pred: "销量" },
+        { name: "金额", operator: "$sum", pred: "金额" },
+      ],
+      groupby: "商品",
+    }}
+    preds={["商品", "金额", "销量", "件单价"]}
+    customRender={{
+      件单价: (v: any, record: any) => <span>{record.金额 / record.销量}</span>,
+    }}
+    options={false}
+  />
+);
