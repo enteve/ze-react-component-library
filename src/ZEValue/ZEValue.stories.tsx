088bf747dc974c93dee789d62985b4ea472bbeae
kilik52 <kilik52@hotmail.com>
2021-07-04 22:53:34 +0800

    feat：ZEValue

---
+++ src/ZEValue/ZEValue.stories.tsx
@@ -0,0 +1,45 @@
+// Generated with util/create-component.js
+import { ArrowUpOutlined } from "@ant-design/icons";
+import { Card, Statistic } from "antd";
+import React from "react";
+import ZEValue from "./index";
+
+// prepare server
+import prepareServerForStories from "../../util/prepareServerForStories";
+prepareServerForStories();
+
+export default {
+  title: "ZEValue",
+};
+
+export const Basic = () => (
+  <>
+    <h2>总订单数量</h2>
+    <div>
+      {ZEValue({
+        schema: "productsale",
+        operator: "$count",
+        pred: "订单编号",
+      })}
+    </div>
+  </>
+);
+
+export const WithAntdCard = () => (
+  <>
+    <Card>
+      <Statistic
+        title="Active"
+        value={ZEValue({
+          schema: "productsale",
+          operator: "$count",
+          pred: "订单编号",
+        })}
+        precision={2}
+        valueStyle={{ color: "#3f8600" }}
+        prefix={<ArrowUpOutlined />}
+        suffix="%"
+      />
+    </Card>
+  </>
+);
