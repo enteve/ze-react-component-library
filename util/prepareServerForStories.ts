9cc27c217d87125c43b346c0f62948a0dc9dffe1
kilik52 <kilik52@hotmail.com>
2021-08-05 19:51:27 +0800

    fix: update token

--- util/prepareServerForStories.ts
+++ util/prepareServerForStories.ts
@@ -1,8 +1,8 @@
 import { config } from "zeroetp-api-sdk";
 
 export default () => {
-  config.API_URL = "http://localhost:3052";
+  config.API_URL = "https://admin.xuetaifeng.com";
   localStorage.token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGRjODcxZWI5N2VlZjMzMzFkZGNiNzkiLCJpYXQiOjE2Mjc1MzAxMzQsImV4cCI6MTYyODEzNDkzNH0.Ckn8mxPiQ2nDoDPeEEwx6OX2AFAJiN374SRy0mpJE_M";
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTBiYzgyNjVlZWQ2ZjRmMmNiZDBkMTQiLCJpYXQiOjE2MjgxNjIwOTIsImV4cCI6MTYyODc2Njg5Mn0.HVgaE3TQQzrZ7-5iF4mWZedj2Of7mibMIWjpvN3Wsr8";
   // config.API_URL = "http://localhost:3052";
 };
