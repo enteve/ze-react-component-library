3471a43dc548a71d2a690a0e92ab2629774534e0
kilik52 <kilik52@hotmail.com>
2021-08-05 15:31:07 +0800

    fix: 又又又又忘记export啦

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import ZETable from "./ZETable";
 import ZEChart from "./ZEChart";
 import ZEAsk from "./ZEAsk";
 import ZESchemaForm from "./ZESchemaForm";
+import ZELogicform from "./ZELogicform";
 
-export { ZEValue, ZETable, ZEChart, ZESchemaForm, ZEAsk };
+export { ZEValue, ZETable, ZEChart, ZESchemaForm, ZELogicform, ZEAsk };
