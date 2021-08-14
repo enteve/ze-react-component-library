41f85123097dac8747f0ce9cc4153a9b76d7ab3d
kilik52 <kilik52@hotmail.com>
2021-08-14 16:21:57 +0800

    fix: typo

--- src/ZESchemaForm/ZESchemaForm.types.ts
+++ src/ZESchemaForm/ZESchemaForm.types.ts
@@ -1,12 +1,20 @@
 import { ProFormColumnsType } from "@ant-design/pro-form";
 import { FormSchema } from "@ant-design/pro-form/lib/components/SchemaForm";
-export type ExtendValueTypes = "percentage" | "object" | "boolean" | "file" | "table";
+export type ExtendValueTypes =
+  | "percentage"
+  | "object"
+  | "boolean"
+  | "file"
+  | "table";
 
 // Generated with util/create-component.js
-export type ZESchemaFromColumnType = ProFormColumnsType<ProFormColumnsType, ExtendValueTypes>;
-export type ZESchemaFromProps = Omit<FormSchema<any>, "columns"> & {
+export type ZESchemaFormColumnType = ProFormColumnsType<
+  ProFormColumnsType,
+  ExtendValueTypes
+>;
+export type ZESchemaFormProps = Omit<FormSchema<any>, "columns"> & {
   schemaID: string;
-  columns?: ZESchemaFromColumnType[];
+  columns?: ZESchemaFormColumnType[];
   propertyConfig?: {
     [key: string]: {
       readonly?: boolean;
