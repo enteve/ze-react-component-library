0e8aec49b2708bea12aa0aec7a09b0857401797a
lilyzhou <lj_zhou@outlook.com>
2021-08-18 16:53:26 +0800

    feat: ZESchemaForm表格类型支持关闭新增按钮

--- util/prepareServerForStories.ts
+++ util/prepareServerForStories.ts
@@ -3,6 +3,6 @@ import { config } from "zeroetp-api-sdk";
 export default () => {
   config.API_URL = "https://admin.xuetaifeng.com";
   localStorage.token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTBiYzgyNjVlZWQ2ZjRmMmNiZDBkMTQiLCJpYXQiOjE2Mjg0OTU3ODQsImV4cCI6MTYyOTEwMDU4NH0.RYw0_iH70byE9JISwAwH76JWPScK05c6GpksoeIIxKM";
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1VVZBR2tvcGZGQks2N1pfTVgtNGIiLCJpYXQiOjE2Mjg4MzkxMzAsImV4cCI6MTYyOTQ0MzkzMH0.bVeUOXLWSBhKZW12JcQJYkoYj4KvvzJdWCcL6OMnjis";
   // config.API_URL = "http://localhost:3052";
 };
