24704096fab9231905eca4b3ec7757c73bea216b
kilik52 <kilik52@hotmail.com>
2021-09-19 16:43:44 +0800

    feat: echarts

---
+++ src/ZEChart/EChart/options/pie.ts
@@ -0,0 +1,14 @@
+export default () => {
+  return {
+    tooltip: {
+      trigger: "item",
+    },
+    series: [
+      {
+        top: 20,
+        type: "pie",
+        data: [],
+      },
+    ],
+  };
+};
