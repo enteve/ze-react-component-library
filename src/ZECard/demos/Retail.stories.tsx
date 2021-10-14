58c0510187ac6b98e6b9043cfd403de6bc45a728
kilik52 <kilik52@hotmail.com>
2021-10-14 12:35:59 +0800

    doc: MultiCardWithCustomContent Bug

--- src/ZECard/demos/Retail.stories.tsx
+++ src/ZECard/demos/Retail.stories.tsx
@@ -1,9 +1,13 @@
 // Generated with util/create-component.js
-import React from "react";
+import React, { useState } from "react";
 import ZECard from "../ZECard";
 import "antd/dist/antd.css";
 
 import prepareServerForStories from "../../../util/prepareServerForStories";
+import { LogicformType } from "zeroetp-api-sdk";
+import { Button } from "antd";
+import ZELogicform from "../../ZELogicform";
+import ZETable from "../../ZETable";
 prepareServerForStories();
 
 export default {
@@ -80,5 +84,77 @@ export const SingleBar = () => {
         groupby: { _id: "产品" },
       }}
     />
+  );
+};
+
+export const MultiCardWithCustomContent = () => {
+  const createMainContent = (logicform) => {
+    return (
+      <ZELogicform
+        logicform={{
+          ...logicform,
+          groupby: undefined,
+        }}
+        content={(v) => <div>{v}</div>}
+      />
+      // <ZECard
+      //   logicform={{
+      //     ...logicform,
+      //     groupby: undefined,
+      //   }}
+      // />
+    );
+  };
+
+  const [logicforms, setLogicforms] = useState<any[]>([
+    {
+      lf: {
+        schema: "sales",
+        preds: [
+          {
+            operator: "$sum",
+            pred: "销售量",
+            name: "总销量",
+          },
+        ],
+        query: {
+          日期: { $offset: { year: 0 } },
+          产品: {
+            operator: "$ent",
+            schema: "product",
+            field: "名称",
+            name: "美丽无敌女长袖",
+          },
+        },
+        groupby: { _id: "产品" },
+      },
+      mainContent: (logicform) => <>{createMainContent(logicform)}</>,
+    },
+  ]);
+
+  const addOneCard = () => {
+    setLogicforms([
+      ...logicforms,
+      {
+        lf: {
+          schema: "product",
+          limit: 1,
+        },
+      },
+    ]);
+  };
+
+  return (
+    <>
+      <Button onClick={() => addOneCard()}>Add One Card</Button>
+      {logicforms.map((item, i) => (
+        <ZECard
+          title="anyway"
+          logicform={item.lf}
+          key={i}
+          mainContent={item.mainContent}
+        />
+      ))}
+    </>
   );
 };
