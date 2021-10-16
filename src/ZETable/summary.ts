7e77ff36ade1b73cf8df538e0c79300ea015ff8f
kilik52 <kilik52@hotmail.com>
2021-10-16 17:30:56 +0800

    feat: 暂时关闭summary

--- src/ZETable/summary.ts
+++ src/ZETable/summary.ts
@@ -1,5 +1,6 @@
 // Add summary row into table
 // 感觉是一个临时的解决方案。要考虑如何解决percentage的问题
+// 20211016暂时关了
 import type { LogicformType, PropertyType } from "zeroetp-api-sdk";
 
 export const addSummaryToData = (
@@ -7,32 +8,33 @@ export const addSummaryToData = (
   logicform: LogicformType,
   columnProperties: PropertyType[]
 ) => {
-  // 一些暂不支持的情况
-  if (!logicform.groupby) return data;
-  if (Array.isArray(logicform.groupby) && logicform.groupby.length > 1)
-    return data;
+  return data;
+  // // 一些暂不支持的情况
+  // if (!logicform.groupby) return data;
+  // if (Array.isArray(logicform.groupby) && logicform.groupby.length > 1)
+  //   return data;
 
-  const newData: any[] = [...data];
+  // const newData: any[] = [...data];
 
-  const total: any = {
-    _id: "__total",
-    [columnProperties[0].name]: "总计",
-  };
+  // const total: any = {
+  //   _id: "__total",
+  //   [columnProperties[0].name]: "总计",
+  // };
 
-  // 总计栏
-  for (const item of data) {
-    for (const property of columnProperties) {
-      if (property.type === "percentage") {
-        // 先不管
-      } else if (property.primal_type === "number") {
-        if (!(property.name in total)) total[property.name] = 0;
+  // // 总计栏
+  // for (const item of data) {
+  //   for (const property of columnProperties) {
+  //     if (property.type === "percentage") {
+  //       // 先不管
+  //     } else if (property.primal_type === "number") {
+  //       if (!(property.name in total)) total[property.name] = 0;
 
-        total[property.name] += item[property.name];
-      }
-    }
-  }
+  //       total[property.name] += item[property.name];
+  //     }
+  //   }
+  // }
 
-  newData.push(total);
+  // newData.push(total);
 
-  return newData;
+  // return newData;
 };
