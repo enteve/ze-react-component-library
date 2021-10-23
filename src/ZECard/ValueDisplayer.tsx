e48251177ae39d4cf95591a9dbd79dc2d98d43c8
kilik52 <kilik52@hotmail.com>
2021-10-23 14:08:56 +0800

    UI: StatisticCard有padding，很垃圾，去掉了

--- src/ZECard/ValueDisplayer.tsx
+++ src/ZECard/ValueDisplayer.tsx
@@ -1,22 +1,14 @@
-import { StatisticCard } from "@ant-design/pro-card";
-import type { StatisticProps } from "@ant-design/pro-card";
+import { Statistic } from "antd";
 import React from "react";
+import numeral from "numeral";
 import { LogicformAPIResultType, LogicformType } from "zeroetp-api-sdk";
-import ZEValue from "../ZEValue";
-
-const { Statistic } = StatisticCard;
 
 type Props = {
   logicform: LogicformType;
   data: LogicformAPIResultType;
-  showRecommender?: boolean;
 };
 
-const ValueDisplayer: React.FC<Props> = ({
-  logicform,
-  data,
-  showRecommender = false,
-}) => {
+const ValueDisplayer: React.FC<Props> = ({ data }) => {
   let unit = "";
   let precision = 1;
   let value = data && "result" in data ? data.result : "-";
@@ -35,44 +27,18 @@ const ValueDisplayer: React.FC<Props> = ({
     if (firstColProp.type === "int") {
       precision = 0;
     }
-  }
 
-  const statistic: StatisticProps = {
-    value,
-    suffix: unit,
-    precision,
-  };
-
-  // Recommender
-  if (showRecommender) {
-    // 目前只对时间的环比做出反应
-    if (logicform.operator === "$sum" && data.schema.type === "event") {
-      const momValue = ZEValue(
-        {
-          ...logicform,
-          operator: "$mom",
-        },
-        (v) => v * 100
-      );
-      const prop: any = {
-        value: momValue,
-      };
-      if (typeof momValue === "number") {
-        prop.value = Math.abs(momValue);
-        if (momValue > 0) {
-          prop.trend = "up";
-        } else if (momValue < 0) {
-          prop.trend = "down";
-        }
-      }
-
-      statistic.description = (
-        <Statistic title="环比" precision={1} suffix="%" {...prop} />
+    if (firstColProp.ui?.formatter) {
+      return (
+        <Statistic
+          value={numeral(value).format(firstColProp.ui?.formatter)}
+          suffix={unit}
+        />
       );
     }
   }
 
-  return <StatisticCard statistic={statistic} />;
+  return <Statistic value={value} suffix={unit} precision={precision} />;
 };
 
 export default ValueDisplayer;
