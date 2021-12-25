d7f63e1e044dcf7ff131f59e5ae49450b7b63923
lilyzhou <lj_zhou@outlook.com>
2021-12-25 16:01:49 +0800

    feat: ZESchemaEditor功能开发

--- util/prepareServerForStories.ts
+++ util/prepareServerForStories.ts
@@ -3,9 +3,9 @@ import { config } from "zeroetp-api-sdk";
 export default () => {
   // config.API_URL = "http://localhost:3052";
   // config.API_URL = "https://zhouheiya.zeroetp.com";
-  config.API_URL = "https://admin.xuetaifeng.com";
-  // config.API_URL = "https://retail.yiwendata.com";
+  // config.API_URL = "https://admin.xuetaifeng.com";
+  config.API_URL = "https://retail.yiwendata.com";
   // config.API_URL = "https://jaem.yiwendata.com";
   localStorage.token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTBiYzgyNjVlZWQ2ZjRmMmNiZDBkMTQiLCJpYXQiOjE2NDAxNzk0NzcsImV4cCI6MTY0MDc4NDI3N30.zGpYG93g_MGYqtdF_Q9ZhuOPjDnyQ61Fc9ZXPm7ogeE";
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWE0ODY4ZjgyMWY3YjQ3YWQ4MzhmNzMiLCJpYXQiOjE2NDAzMzU3NzMsImV4cCI6MTY1MDcwMzc3M30.E7RwNNloNujiANrYPTplYpWTuxgH4vW4lrC3wWzKhf8";
 };
