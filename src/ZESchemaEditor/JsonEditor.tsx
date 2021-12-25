f89b1b8ca326ec7dd6023689e9943ff77f5a3845
lilyzhou <lj_zhou@outlook.com>
2021-12-25 16:01:49 +0800

    feat: ZESchemaEditor功能开发

---
+++ src/ZESchemaEditor/JsonEditor.tsx
@@ -0,0 +1,264 @@
+import React, { FC, useRef, useEffect } from "react";
+import JSONEditor from "jsoneditor";
+import "jsoneditor/dist/jsoneditor.css";
+
+export const types = ["entity", "event"] as const;
+export type Type = typeof types[number];
+
+type JsonEditorProps = {
+  json?: Record<string, any>;
+  editorRef?: React.MutableRefObject<any>;
+  editable: boolean;
+  type: Type;
+};
+const modes = ["tree", "code"] as const;
+
+const propertySchema = {
+  type: "object",
+  properties: {
+    name: {
+      type: "string",
+    },
+    syno: {
+      type: "array",
+      items: {
+        type: "string",
+      },
+    },
+    type: {
+      type: "string",
+    },
+    is_name: {
+      type: "boolean",
+    },
+    is_categorical: {
+      type: "boolean",
+    },
+    ref: {
+      type: "string",
+    },
+    unit: {
+      type: "string",
+    },
+    constraints: {
+      type: "object",
+      properties: {
+        required: {
+          type: "boolean",
+        },
+        enum: {
+          type: "array",
+          items: {
+            type: ["string", "array", "boolean"],
+          },
+        },
+      },
+    },
+    ui: {
+      type: "object",
+      properties: {
+        formatter: {
+          type: "string",
+        },
+        formatters: {
+          type: "array",
+          items: {
+            type: "object",
+            properties: {
+              formatter: {
+                type: "string",
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  anyOf: [
+    {
+      not: {
+        properties: {
+          type: { const: "object" },
+        },
+        required: ["name"],
+      },
+    },
+    {
+      required: ["name", "ref"],
+    },
+  ],
+};
+
+const getSchema = (json?: Record<string, any>) => {
+  const scheme = {
+    type: "object",
+    properties: {
+      _id: {
+        type: "string",
+        // 更新时，设定enum，不允许修改
+        enum: json ? [json?._id] : undefined,
+      },
+      name: {
+        type: "string",
+      },
+      syno: {
+        type: "array",
+        items: {
+          type: "string",
+        },
+      },
+      type: {
+        type: "string",
+        enum: json ? [json.type] : ["entity", "event"],
+      },
+      description: {
+        type: "string",
+      },
+      editable: {
+        type: "boolean",
+      },
+      use_db_date_as_mtd: {
+        type: "boolean",
+      },
+      use_view: {
+        type: "boolean",
+      },
+      properties: {
+        type: "array",
+        items: propertySchema,
+        minItems: 1,
+      },
+      hierarchy: {
+        type: "array",
+        items: {
+          type: "object",
+          properties: {
+            name: {
+              type: "string",
+            },
+            syno: {
+              type: "array",
+              items: {
+                type: "string",
+              },
+            },
+            code_length: {
+              type: "number",
+            },
+          },
+          required: ["name", "code_length"],
+        },
+      },
+    },
+    required: ["_id", "name", "type"],
+  };
+
+  return scheme;
+};
+
+// 初始值设置成null，需要手动输入，否则校验不通过
+const propertyTemplate = {
+  name: null,
+  syno: [],
+  description: "",
+  type: null,
+  is_name: false,
+  is_categorical: false,
+  ref: null,
+  unit: null,
+  constraints: {
+    required: true,
+    enum: [],
+  },
+  ui: {
+    formatter: null,
+    formatters: [
+      {
+        formatter: null,
+      },
+    ],
+  },
+};
+
+const hierarchyTemplate = {
+  name: null,
+  syno: [],
+  code_length: null,
+};
+
+export const schemaTemplate = {
+  _id: null,
+  name: null,
+  syno: [],
+  type: "entity",
+  description: "",
+  editable: false,
+  use_db_date_as_mtd: false,
+  use_view: false,
+  properties: [propertyTemplate],
+  hierarchy: [hierarchyTemplate],
+};
+
+const JsonEditor: FC<JsonEditorProps> = ({
+  json,
+  editorRef: editorInstanceRef,
+  editable,
+  type,
+}) => {
+  const containerRef = useRef<HTMLDivElement>();
+  const editorRef = useRef<any>();
+  const editableRef = useRef<boolean>(true);
+  editableRef.current = editable;
+
+  useEffect(() => {
+    if (editorRef.current) {
+      editorRef.current.setSchema(getSchema(json));
+      editorRef.current.set(json || { ...schemaTemplate, type });
+    }
+  }, [json]);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      editorRef.current = new JSONEditor(
+        containerRef.current,
+        {
+          mode: "code",
+          // 传入modes可开启模式切换
+          modes,
+          schema: getSchema(json),
+          onEditable: () => {
+            return editableRef.current;
+          },
+          templates: [
+            {
+              text: "property",
+              title: "新增一个属性",
+              field: "properties",
+              value: propertyTemplate,
+            },
+            {
+              text: "hierarchy",
+              title: "新增一个hierarchy",
+              field: "hierarchy",
+              value: hierarchyTemplate,
+            },
+          ],
+        },
+        json || { ...schemaTemplate, type }
+      );
+      if (editorInstanceRef) {
+        editorInstanceRef.current = editorRef.current;
+      }
+    }
+  }, []);
+
+  return (
+    <div
+      className="ze-json-editor"
+      ref={containerRef}
+      style={{ height: "100%" }}
+    />
+  );
+};
+
+export default JsonEditor;
--- src/ZESchemaEditor/ZESchemaEditor.tsx
+++ src/ZESchemaEditor/ZESchemaEditor.tsx
@@ -1,7 +1,225 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import { Button, Spin, Drawer, Space, message, Popconfirm, Radio } from "antd";
+import {
+  getSchemas,
+  createSchema,
+  updateSchema,
+  SchemaType,
+  deleteSchema,
+} from "zeroetp-api-sdk";
+import { useRequest } from "@umijs/hooks";
+import ProTable from "@ant-design/pro-table";
+import { request } from "../request";
+import JsonEditor, { types, Type } from "./JsonEditor";
 
 const ZESchemaEditor: React.FC = () => {
-  return <div>hello</div>;
+  const {
+    data: schemas = [],
+    run: fetchSchemas,
+    loading,
+  } = useRequest(getSchemas, {
+    formatResult: (res: any) => res?.schemas || [],
+  });
+  const [drawerVisible, setDrawerVisible] = useState<boolean>(false);
+  const [editingRecord, setEditingRecord] = useState<SchemaType>();
+  const editorRef = useRef<any>();
+  const [saveLoading, setSaveLoading] = useState<boolean>(false);
+  const [mode, setMode] = useState<"edit" | "detail">("edit");
+  const [type, setType] = useState<Type>("entity");
+
+  const onCancel = () => {
+    setMode("edit");
+    setDrawerVisible(false);
+    setEditingRecord(null);
+    if (editorRef.current) {
+      editorRef.current.setMode("tree");
+    }
+  };
+
+  const onFinish = async () => {
+    if (editorRef.current) {
+      const errors: any[] = await editorRef.current.validate();
+      if (errors && errors.length > 0) {
+        message.error("schema校验不通过，请检查！");
+        return;
+      }
+      const newJson = editorRef.current.get();
+      setSaveLoading(true);
+      let res;
+      if (editingRecord) {
+        const payload = { ...newJson };
+        delete payload._id;
+        res = await request(updateSchema(editingRecord._id, payload));
+      } else {
+        res = await request(createSchema(newJson));
+      }
+
+      setSaveLoading(false);
+      if (res) {
+        message.success("保存成功");
+        onCancel();
+        fetchSchemas();
+      } else {
+        message.error("保存失败，请重试");
+      }
+    }
+  };
+
+  const onDelete = async (id?: string) => {
+    if (id) {
+      const res = await request(deleteSchema(id));
+      if (res) {
+        message.success("删除成功");
+        fetchSchemas();
+      } else {
+        message.error("删除失败，请重试！");
+      }
+    }
+  };
+
+  const columns = [
+    {
+      title: "_id",
+      dataIndex: "_id",
+    },
+    {
+      title: "name",
+      dataIndex: "name",
+    },
+    {
+      title: "type",
+      dataIndex: "type",
+    },
+    {
+      title: "syno",
+      dataIndex: "syno",
+    },
+    {
+      title: "description",
+      dataIndex: "description",
+    },
+  ]
+    .map((d) => ({ ...d, width: 200 }))
+    .concat([
+      {
+        title: "操作",
+        fixed: "right",
+        width: 140,
+        valueType: "option",
+        render: (text, record) => [
+          <a
+            key="detail"
+            onClick={() => {
+              setMode("detail");
+              setDrawerVisible(true);
+              setEditingRecord(record);
+            }}
+          >
+            详情
+          </a>,
+          <a
+            key="edit"
+            onClick={() => {
+              setMode("edit");
+              setDrawerVisible(true);
+              setEditingRecord(record);
+            }}
+          >
+            编辑
+          </a>,
+          <Popconfirm
+            title="是否确定删除"
+            key="delete"
+            onConfirm={() => {
+              onDelete(record?._id);
+            }}
+          >
+            <Button type="link" danger style={{ padding: 0 }}>
+              删除
+            </Button>
+          </Popconfirm>,
+        ],
+      } as any,
+    ]);
+
+  useEffect(() => {
+    if (editorRef.current && drawerVisible) {
+      editorRef.current.setMode("code");
+    }
+  }, [drawerVisible]);
+
+  return (
+    <Spin spinning={loading}>
+      <ProTable<SchemaType>
+        toolBarRender={() => {
+          return [
+            <Radio.Group
+              key="type"
+              options={types.map((d) => ({ label: d, value: d }))}
+              onChange={(e) => {
+                setType(e.target.value);
+              }}
+              value={type}
+              optionType="button"
+            />,
+            <Button
+              key="add"
+              type="primary"
+              onClick={() => {
+                setMode("edit");
+                setDrawerVisible(true);
+                setEditingRecord(null);
+              }}
+            >
+              新建
+            </Button>,
+          ];
+        }}
+        rowKey="_id"
+        headerTitle="Schema列表"
+        dataSource={schemas.filter((d) => d.type === type)}
+        columns={columns}
+        search={false}
+        options={false}
+        scroll={{ x: columns.reduce((p, c) => p + c.width, 0) }}
+        pagination={false}
+      />
+      <Drawer
+        visible={drawerVisible}
+        width={800}
+        maskClosable={mode === "edit" ? false : true}
+        onClose={onCancel}
+        title={
+          mode === "edit"
+            ? editingRecord
+              ? "更新Schema"
+              : "新增Schema"
+            : "Schema详情"
+        }
+        footer={
+          mode === "edit" ? (
+            <div style={{ display: "flex", flexDirection: "row-reverse" }}>
+              <Space>
+                <Button onClick={onCancel}>取消</Button>
+                <Button type="primary" onClick={onFinish} loading={saveLoading}>
+                  确定
+                </Button>
+              </Space>
+            </div>
+          ) : (
+            false
+          )
+        }
+      >
+        <JsonEditor
+          json={editingRecord}
+          editorRef={editorRef}
+          editable={mode === "edit"}
+          type={type}
+        />
+      </Drawer>
+    </Spin>
+  );
 };
 
 export default ZESchemaEditor;
