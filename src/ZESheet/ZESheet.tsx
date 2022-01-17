09cca226d810e0ea67f745329ee11f3ab94349f1
lilyzhou <lj_zhou@outlook.com>
2022-01-17 20:20:50 +0800

    feat[ZESheet]: 支持formatter

--- src/ZESheet/ZESheet.tsx
+++ src/ZESheet/ZESheet.tsx
@@ -3,6 +3,7 @@ import { ZESheetProps } from "./ZESheet.types";
 import { SheetComponent } from "@antv/s2-react";
 import { LogicformAPIResultType } from "zeroetp-api-sdk";
 import { useRequest } from "@umijs/hooks";
+import { findProperty, formatWithProperty } from "../util";
 import { requestLogicform } from "../request";
 import { Result, Skeleton } from "antd";
 import flatten from "flat";
@@ -43,8 +44,33 @@ const ZESheet: React.FC<ZESheetProps> = ({
       return <Result title="此组件仅支持数组" />;
     }
 
+    const meta: Required<S2DataConfig>["meta"] = ["rows", "columns", "values"]
+      .reduce((p, r) => p.concat(dataCfg.fields[r] || []), [])
+      .map((d) => {
+        const property = findProperty(
+          { ...data.schema, properties: data.columnProperties },
+          d
+        );
+        const metaOfProps = dataCfg.meta?.find((f) => f.field === d);
+        const defaultFormatter = (v) => {
+          return formatWithProperty(property, v);
+        };
+        if (metaOfProps) {
+          return {
+            ...metaOfProps,
+            formatter: metaOfProps.formatter || defaultFormatter,
+          };
+        }
+        return {
+          field: d,
+          formatter: defaultFormatter,
+        };
+      });
+
     // flatten result
     dataCfg.data = data.result.map((v) => flatten(v));
+    // meta
+    dataCfg.meta = meta;
   }
 
   return (
