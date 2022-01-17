5019325f1ad9a7962ccbdc900cbf45f55d585a73
kilik52 <kilik52@hotmail.com>
2022-01-17 10:32:43 +0800

    feat: ZESheet Basic

--- src/ZESheet/ZESheet.tsx
+++ src/ZESheet/ZESheet.tsx
@@ -2,18 +2,62 @@ import React from "react";
 import { ZESheetProps } from "./ZESheet.types";
 import "@antv/s2-react/dist/style.min.css";
 import { SheetComponent } from "@antv/s2-react";
+import { LogicformAPIResultType } from "zeroetp-api-sdk";
+import { useRequest } from "@umijs/hooks";
+import { requestLogicform } from "../request";
+import { Result, Skeleton } from "antd";
+import flatten from "flat";
+import { S2DataConfig } from "@antv/s2";
+import { DownloadOutlined } from "@ant-design/icons";
 
 const ZESheet: React.FC<ZESheetProps> = ({
+  logicform,
+  result,
   sheetType,
   s2DataConfig,
-  s2Options,
+  s2Options = {
+    width: undefined,
+    height: 480,
+  },
+  showExport = true,
 }) => {
+  const { data, loading } = useRequest<LogicformAPIResultType>(
+    () => {
+      if (result) {
+        return new Promise((resolve) => resolve(result));
+      }
+
+      return requestLogicform(logicform);
+    },
+    {
+      refreshDeps: [logicform, result],
+    }
+  );
+
+  const dataCfg: S2DataConfig = {
+    fields: undefined,
+    data: [],
+    ...s2DataConfig,
+  };
+  if (data?.result) {
+    if (!Array.isArray(data.result)) {
+      return <Result title="此组件仅支持数组" />;
+    }
+
+    // flatten result
+    dataCfg.data = data.result.map((v) => flatten(v));
+  }
+
   return (
-    <SheetComponent
-      sheetType={sheetType}
-      dataCfg={s2DataConfig}
-      options={s2Options}
-    />
+    <Skeleton loading={loading}>
+      <SheetComponent
+        adaptive
+        dataCfg={dataCfg}
+        options={s2Options}
+        sheetType={sheetType}
+        header={{ exportCfg: { open: showExport, icon: <DownloadOutlined /> } }}
+      />
+    </Skeleton>
   );
 };
 export default ZESheet;
--- src/ZESheet/ZESheet.types.ts
+++ src/ZESheet/ZESheet.types.ts
@@ -1,7 +1,11 @@
 import { S2Options, S2DataConfig } from "@antv/s2";
+import { LogicformAPIResultType, LogicformType } from "zeroetp-api-sdk";
 
 export interface ZESheetProps {
+  logicform: LogicformType;
+  result?: LogicformAPIResultType;
   sheetType?: "pivot" | "table" | "gridAnalysis";
-  s2DataConfig: S2DataConfig;
-  s2Options: S2Options;
+  s2DataConfig: Omit<S2DataConfig, "data">;
+  s2Options?: S2Options;
+  showExport?: boolean;
 }
