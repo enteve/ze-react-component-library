7bc66c9a076f68bd54b38ffc474cb8f03fd1e202
lilyzhou <lj_zhou@outlook.com>
2022-01-23 16:31:40 +0800

    fix: pro components升级后proFieldKey丢失

--- util/prepareServerForStories.ts
+++ util/prepareServerForStories.ts
@@ -3,9 +3,9 @@ import { config } from "zeroetp-api-sdk";
 export default () => {
   // config.API_URL = "http://localhost:3052";
   // config.API_URL = "https://zhouheiya.zeroetp.com";
-  // config.API_URL = "https://admin.xuetaifeng.com";
-  config.API_URL = "https://retail.yiwendata.com";
+  config.API_URL = "https://admin.xuetaifeng.com";
+  // config.API_URL = "https://retail.yiwendata.com";
   // config.API_URL = "https://jaem.yiwendata.com";
   localStorage.token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWQzYWNiMDY4Njc5NmU1NTg3ZjBmNjAiLCJpYXQiOjE2NDEyNjIzMzIsImV4cCI6MTY1MTYzMDMzMn0.E7l_-oGE7HG0U1XQh9Uw051riSu8eb8tncFlBBupsqs";
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTBiYzgyNjVlZWQ2ZjRmMmNiZDBkMTQiLCJpYXQiOjE2NDI5MjMxNTYsImV4cCI6MTY1MzI5MTE1Nn0.IRAPfjUotUu_1SW31AsK0DvB3XDwh8rOAVBMGCz1eQk";
 };
