3455e6636a504a2dc855467c583fb1e2a92393ff
kilik52 <kilik52@hotmail.com>
2022-02-18 15:59:31 +0800

    chore: update s2

--- src/ZESheet/demos/ZESheet.stories.tsx
+++ src/ZESheet/demos/ZESheet.stories.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import ZESheet from "../index";
 import "./index.less";
+import numeral from "numeral";
 
 // prepare server
 import prepareServerForStories from "../../../util/prepareServerForStories";
@@ -103,5 +104,68 @@ export const ZESheetWithFormatter = () => {
         values.s2Options && setS2Options(values.s2Options);
       }}
     />
+  );
+};
+
+export const TMP = () => {
+  const [lf, setLF] = useState<LogicformType>({
+    schema: "ddi_sales",
+    groupby: ["产品", "市场分类", "终端类型"],
+    query: { 日期: "MTD" },
+    preds: [{ name: "总销量", operator: "$sum", pred: "数量" }],
+  });
+  const [s2DataConfig, setS2DataConfig] = useState<any>({
+    fields: {
+      rows: ["市场分类", "终端类型"],
+      columns: ["产品.名称"],
+      values: ["总销量"],
+      valueInCols: true,
+    },
+    meta: [
+      {
+        field: "总销量",
+        formatter: (v) => numeral(v / 1000).format("0,0.0"),
+      },
+      {
+        field: "市场分类",
+        formatter: (v) => {
+          if (v === "Others") return "其他";
+          return v;
+        },
+      },
+    ],
+    sortParams: [
+      {
+        sortFieldId: "市场分类",
+        sortBy: ["核心市场", "CSO市场", "多渠道", "Others"],
+      },
+      {
+        sortFieldId: "终端类型",
+        sortBy: ["医院进药", "药店", "电商", "互联网医院"],
+      },
+    ],
+  });
+  const [s2Options, setS2Options] = useState<any>({
+    hierarchyType: "tree",
+    hierarchyCollapse: true,
+    totals: {
+      row: {
+        showGrandTotals: true,
+        reverseLayout: true,
+        showSubTotals: true,
+        reverseSubLayout: true,
+        subTotalsDimensions: ["市场分类"],
+        calcTotals: {
+          aggregation: "SUM",
+        },
+        calcSubTotals: {
+          aggregation: "SUM",
+        },
+      },
+    },
+  });
+
+  return (
+    <ZESheet logicform={lf} s2DataConfig={s2DataConfig} s2Options={s2Options} />
   );
 };
