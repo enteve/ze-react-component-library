c2541901a20f5442b23188ba2b655cbdc78a8ced
lilyzhou <lj_zhou@outlook.com>
2022-03-15 18:15:43 +0800

    chore: 新增analyze命令

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,34 +1,44 @@
+import { visualizer } from "rollup-plugin-visualizer";
 const type = process.env.BUILD_TYPE;
+const ANALYZE = process.env.ANALYZE;
 
 let config = {};
 
-if (type === 'lib') {
+if (type === "lib") {
   config = {
-    cjs: { type: 'babel', lazy: true },
+    cjs: { type: "babel", lazy: true },
     esm: false,
     extraBabelPlugins: [
       [
-        'babel-plugin-import',
-        { libraryName: 'antd', libraryDirectory: 'lib', style: true },
-        'antd',
+        "babel-plugin-import",
+        { libraryName: "antd", libraryDirectory: "lib", style: true },
+        "antd",
       ],
     ],
   };
 }
 
-if (type === 'es') {
+if (type === "es") {
   config = {
     cjs: false,
     esm: {
-      type: 'babel',
+      type: "babel",
     },
     extraBabelPlugins: [
       [
-        'babel-plugin-import',
-        { libraryName: 'antd', libraryDirectory: 'es', style: true },
-        'antd',
+        "babel-plugin-import",
+        { libraryName: "antd", libraryDirectory: "es", style: true },
+        "antd",
       ],
     ],
+  };
+}
+
+if (ANALYZE === "1") {
+  config = {
+    cjs: true,
+    esm: false,
+    extraRollupPlugins: [visualizer({ open: true })],
   };
 }
 
