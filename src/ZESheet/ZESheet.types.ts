1f3be9c65bf9ff05313aef2ec984715c4bbbd0b3
lilyzhou <lj_zhou@outlook.com>
2022-04-18 17:58:40 +0800

    feat[ZESheet]: 支持导出excel

--- src/ZESheet/ZESheet.types.ts
+++ src/ZESheet/ZESheet.types.ts
@@ -8,6 +8,7 @@ export interface ZESheetProps {
   sheetType?: "pivot" | "table" | "gridAnalysis";
   s2DataConfig?: Omit<S2DataConfig, "data">;
   s2Options?: Partial<S2Options>;
+  xlsx?: any; // 外链的xlsx库。因为是可选的库，所以用外链的形式去做
   showExport?: boolean;
   showEditor?: boolean; // 是否支持编辑模式
   style?: React.CSSProperties;
