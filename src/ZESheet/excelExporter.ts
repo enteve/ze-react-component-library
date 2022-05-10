6efe0bb1f0c415633a55fe93ecc2dcbea9c9f342
lilyzhou <lj_zhou@outlook.com>
2022-05-10 17:55:06 +0800

    feat[ZESheet]: 导出数据格式化逻辑完善

--- src/ZESheet/excelExporter.ts
+++ src/ZESheet/excelExporter.ts
@@ -1,7 +1,28 @@
 import { message } from "antd";
 
+function getFormatter(text: string) {
+  const unit = text.replaceAll(/[0-9.,]/g, "");
+  const bit = text
+    .split(".")[1]
+    ?.replace(unit, "")
+    .split("")
+    .reduce((p) => `${p}0`, "");
+  let z;
+  if (bit) {
+    z = `0.${bit}`;
+  }
+  if (text.includes(",")) {
+    z = z ? `0,${z}` : "0,0";
+  }
+  if (unit) {
+    z = z ? `${z}${unit}` : `0${unit}`;
+  }
+  return z;
+}
+
 function excelExporter(
   text: string,
+  formattedText: string,
   filename: string,
   XLSX?: any // 外部的XLSX库
 ) {
@@ -11,14 +32,21 @@ function excelExporter(
     return message.error("导出的文件必须设定文件名");
   }
   const excelName = `${filename}.xlsx`;
-  const arr = text.split("\r\n").map((d) =>
-    d.split("\t").map((s) => {
+  const formattedArr = formattedText
+    .split("\r\n")
+    .map((d) => d.split("\t").map((s) => s.slice(1, -1)));
+
+  const arr = text.split("\r\n").map((d, i) =>
+    d.split("\t").map((s, ii) => {
+      // 去除多余的双引号
       const v = s.slice(1, -1);
+      const formattedV: string = formattedArr[i][ii];
       const n = Number(v);
-      if (isNaN(n)) {
+      if (v.trim().length === 0 || isNaN(n)) {
         return v;
       }
-      return { v: n, t: "n", z: Math.abs(n) < 1 ? "0.00%" : "0,00" };
+      const z = getFormatter(formattedV);
+      return { v: n, t: "n", z };
     })
   );
   const worksheet = XLSX.utils.aoa_to_sheet(arr);
