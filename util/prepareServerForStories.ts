d1bdc0f825be1080fc2099045626e8a13fe2c137
lilyzhou <lj_zhou@outlook.com>
2022-05-16 19:23:42 +0800

    fix[ZEDashboard]: getDashboardState方法获取的值不是最新

--- util/prepareServerForStories.ts
+++ util/prepareServerForStories.ts
@@ -7,5 +7,5 @@ export default () => {
   config.API_URL = "https://retail.yiwendata.com";
   // config.API_URL = "https://jaem.yiwendata.com";
   localStorage.token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjczOTViYTA5NDM2ZmViY2IxNTlhYjAiLCJpYXQiOjE2NTE3NDIyNTQsImV4cCI6MTY2MjExMDI1NH0.XC7rDVqkXz8SX4atXruMyrFF3cQ1t_XJMBMTY9TN_E0";
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjgyMWNlMDU4YWUzZWM0OGNlNTU2NTEiLCJpYXQiOjE2NTI2OTc2MjMsImV4cCI6MTY2MzA2NTYyM30.UYd_kW53Dd6OREwFS_rwZCDYWe-qSS8_ZKNEgSFp9Fs";
 };
