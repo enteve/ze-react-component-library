27e9fca4c84b9cb0b3800c026b82e18915ecf0c9
kilik52 <kilik52@hotmail.com>
2022-05-16 22:23:51 +0800

    feat[ZESearchBar]: initialValue

--- src/ZESearchBar/demos/ZESearchBar.stories.tsx
+++ src/ZESearchBar/demos/ZESearchBar.stories.tsx
@@ -17,3 +17,13 @@ export const ZESearchBarDemo = () => {
     </>
   );
 };
+
+export const InitialValue = () => {
+  const [answer, setAnswer] = useState<ZESearchBarAnswerType>();
+  return (
+    <>
+      <ZESearchBar onAsk={setAnswer} initialValue="今年销售额" />
+      {answer && <pre>{JSON.stringify(answer, null, 2)}</pre>}
+    </>
+  );
+};
