54d29d7f99234b2a99fd2ff310c5e5eb00ac0828
lilyzhou <lj_zhou@outlook.com>
2022-05-20 19:24:24 +0800

    fix[ZETable]：reload的失效

--- src/ZECard/Table/Table.tsx
+++ src/ZECard/Table/Table.tsx
@@ -307,7 +307,6 @@ const Table: React.FC<TableProps> = ({
 
   const [creationFormVisible, setCreationFormVisible] =
     useState<boolean>(false);
-  const tableRef = useRef<ActionType>();
   const [rowChildrenMap, setRowChildrenMap] = useState<Record<string, any[]>>(
     {}
   );
@@ -531,7 +530,6 @@ const Table: React.FC<TableProps> = ({
       bodyStyle: { padding: 0 },
     },
     ...restProps,
-    actionRef: tableRef,
     columns,
     rowKey,
     search: search === undefined ? false : search,
@@ -635,7 +633,7 @@ const Table: React.FC<TableProps> = ({
   const deleteRecord = (record: any) => {
     if (logicform.schema) {
       requestAPI(removeDataByID(logicform.schema, record._id)).then(() => {
-        tableRef.current.reload();
+        reload?.();
       });
     }
   };
@@ -692,11 +690,13 @@ const Table: React.FC<TableProps> = ({
           // TODO: 目前只接受非chain的query
           requestAPI(
             createData(logicform.schema, { ...logicform.query, ...record })
-          ).then(() => tableRef.current.reload());
+          ).then(() => {
+            reload?.();
+          });
         } else {
-          requestAPI(updateDataByID(logicform.schema, id, record)).then(() =>
-            tableRef.current.reload()
-          );
+          requestAPI(updateDataByID(logicform.schema, id, record)).then(() => {
+            reload?.();
+          });
         }
       },
       onDelete: (_id, record) => deleteRecord(record),
@@ -811,7 +811,7 @@ const Table: React.FC<TableProps> = ({
                   );
                 }
                 setCreationFormVisible(false);
-                tableRef.current.reload();
+                reload?.()
               }}
             />
           </Drawer>
