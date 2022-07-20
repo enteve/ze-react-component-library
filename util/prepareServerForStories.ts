49712689bd5e4c688ce029ec02df49b9f241bf22
kilik52 <kilik52@hotmail.com>
2022-07-20 22:29:31 +0800

    fix: 安全考虑。去掉token

--- util/prepareServerForStories.ts
+++ util/prepareServerForStories.ts
@@ -1,8 +1,7 @@
-// 复制一份出来，改名为prepareServerForStories.ts才可以启动系统
+// 要在浏览器的localStorage里面填入token
 import { config } from "zeroetp-api-sdk";
 
 export default () => {
   config.API_URL = "http://localhost:3052";
   // config.API_URL = "https://retail.yiwendata.com";
-  localStorage.token =  "";
 };
