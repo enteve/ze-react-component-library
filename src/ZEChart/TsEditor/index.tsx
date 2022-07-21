907a2bd58b2df85c51a962afd9717f986d9b8d8a
lilyzhou <lj_zhou@outlook.com>
2022-07-21 16:40:34 +0800

    feat: ZEChart编辑时，默认options第一层折叠起来

--- src/ZEChart/TsEditor/index.tsx
+++ src/ZEChart/TsEditor/index.tsx
@@ -1,12 +1,18 @@
-import React, { FC } from "react";
+import React, { FC, useRef, useEffect } from "react";
 import AceEditor, { IAceEditorProps } from "react-ace";
 import "ace-builds/src-noconflict/mode-typescript";
 import "ace-builds/src-noconflict/theme-github";
 import "ace-builds/src-noconflict/ext-language_tools";
 
 type TsEditorProps = Partial<IAceEditorProps>;
+export type AceEditorType = Parameters<IAceEditorProps["onLoad"]>[0];
 
-const TsEditor: FC<TsEditorProps> = ({ name = "ze-ts-editor", ...restProps }) => {
+const TsEditor: FC<TsEditorProps> = ({
+  name = "ze-ts-editor",
+  ...restProps
+}) => {
+  const editorRef = useRef<AceEditorType>();
+
   return (
     <AceEditor
       mode="typescript"
@@ -15,6 +21,11 @@ const TsEditor: FC<TsEditorProps> = ({ name = "ze-ts-editor", ...restProps }) =>
       showPrintMargin={true}
       showGutter={true}
       highlightActiveLine={true}
+      onLoad={(editor) => {
+        editorRef.current = editor;
+        //@ts-ignore 默认第一层折叠起来
+        editor.session.foldAll(1, undefined, 0);
+      }}
       setOptions={{
         enableBasicAutocompletion: true,
         enableLiveAutocompletion: true,
